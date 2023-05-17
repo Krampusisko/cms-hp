1c2c062678ec64223f0d1a0c6415448577f25c2b
Krampusisko <100939289+Krampusisko@users.noreply.github.com>
2023-05-17 14:56:42 +0200

    Usuwam syf

--- cms-hp/src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('./../vendor/autoload.php');
-$db = new mysqli("localhost", "root", "", "cms");
-require("Post.class.php");
-//loader to taki pomocnik do ładowania szablonów
-$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
-//inicjujemy twiga
-$twig = new Twig\Environment($loader);
-?>
